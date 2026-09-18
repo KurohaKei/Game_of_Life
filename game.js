4dd33d7d744fefb12cc8aa63edf8ecd3335f93cb
Kei <kurohakzel@gmail.com>
2026-09-19 03:30:59 +0800

    feat: implement Game of Life rules and cell evolution

--- game.js
+++ game.js
@@ -11,10 +11,10 @@ function make2DArray (cols, rows) {
 let grid;
 let rows;
 let cols;
-let resolution = 40;
+let resolution = 20;
 
 function setup () {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
     cols = width / resolution;
     rows = height / resolution;
 
@@ -29,6 +29,7 @@ function setup () {
 
 function draw () {
     background(0);
+
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let x = i * resolution;
@@ -40,4 +41,37 @@ function draw () {
             }
         }
     }
+    let next = make2DArray(cols, rows);
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+            
+            let sum = 0;
+            let neighbors = countNeighbors(grid, i, j);
+
+            let state = grid[i][j];
+            if (state == 0 && neighbors == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+
+            }
+        }
+    }     
+}
+
+grid = next;
+
+function countNeighbors (grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
 }
