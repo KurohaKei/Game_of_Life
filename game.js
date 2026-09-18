306aaf24bd7826acd43ba50bb917fb02e07ee639
Kei <kurohakzel@gmail.com>
2026-09-19 03:18:14 +0800

    feat: integrate p5.js and fix canvas rendering

--- game.js
+++ game.js
@@ -1,27 +1,43 @@
 console.log("script started");
 
-function make2DArray (rows, cols) {
-    let arr = new Array(rows);
+function make2DArray (cols, rows) {
+    let arr = new Array(cols);
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = new Array(cols);
+        arr[i] = new Array(rows);
     }
     return arr;
 }
 
 let grid;
-let rows = 10;
-let cols = 10;
+let rows;
+let cols;
+let resolution = 40;
 
 function setup () {
+    createCanvas(400, 400);
+    cols = width / resolution;
+    rows = height / resolution;
 
-    grid = make2DArray(rows, cols);
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            grid[i][j] = Math.floor(Math.random() * 2);
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random() * 2);
         }
 
     }
 }
 
-setup();
-console.log(grid);
+function draw () {
+    background(0);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(255);
+                stroke(0);
+                rect(x, y, resolution - 1, resolution - 1);
+            }
+        }
+    }
+}
